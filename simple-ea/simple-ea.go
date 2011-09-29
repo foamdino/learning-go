d25ae0cb9fa1efd86a08d9d784b1563c6c3b9ec6
Kev Jackson <foamdino@gmail.com>
2011-09-29 16:30:48 +0700

    adding the start of a simple evolutionary algorithm

---
+++ simple-ea/simple-ea.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"strconv"
+	"fmt"
+	"math"
+	"rand"
+)
+
+func ord(s string) (val int) {
+	val, err := strconv.Atoi(s)
+	if err != nil {
+		val = 0
+	}
+	return
+}
+
+func randomPlusMinus() (val int) {
+	switch rand.Int31n(3) {
+	case 0:
+		val = 0
+	case 1:
+		val = 1
+	case 2:
+		val = -1
+	}
+	return
+}
+
+
+func mutate(src string) (mutated string) {
+	charpos := rand.Int31n(int32(len(src)))
+	temp := []byte(src)
+	temp[charpos] = uint8(int(src[charpos]) + randomPlusMinus())
+	mutated = string(temp)
+	return
+}
+
+func fitness(src string, target string) (fitval float64) {
+	fitval = 0
+	for pos, _ := range(src) {
+		fitval += math.Pow((float64(target[pos]) - float64(src[pos])), float64(2))
+	}
+	
+	return
+}
+
+func main() {
+	var source = "jiKnp4bqpmAbp"
+	var target = "Hello, World!"
+
+	//fmt.Printf("fitness: %f\n", fitness("Hello", "Hfllo"))
+	//fmt.Printf("original: %s\tmutated: %s\n", "hello", mutate("hello"))
+
+	fitval := int(fitness(source, target))
+	i := 0
+	for fitval > 0 {
+		i += 1
+		m := mutate(source)
+		var fitval_m = int(fitness(m, target))
+		if fitval_m < fitval {
+			fitval = fitval_m
+			source = m
+			fmt.Printf("generation: %d %s[%d]\n", i, m, fitval)
+		}
+	}
+}
