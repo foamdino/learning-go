e005ec98a40307f295e71588353fe85ca93e6329
Kev Jackson <foamdino@gmail.com>
2013-04-23 13:48:30 +0700

    adding a simple appengine example

---
+++ appengine/helloworld/hello/hello.go
@@ -0,0 +1,33 @@
+package hello
+
+import (
+	"fmt"
+	"appengine"
+	"appengine/user"
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	u := user.Current(c)
+
+	if u == nil {
+		url, err := user.LoginURL(c, r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Location", url)
+		w.WriteHeader(http.StatusFound)
+		return
+	}
+
+
+	fmt.Fprintf(w, "Hello %v!", u)
+
+}
